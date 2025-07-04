a283f05bf41354a6f012f14a2c88376033e075d2
AgustinReveles <agustin.reveles@hotmail.com>
2025-07-03 21:46:55 -0300

    Correccion en controladores

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,38 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Tareas;
-use App\Models\Categorias;
-use App\Models\Comentarios;
-use Illuminate\Support\Facades\Http;
-
 abstract class Controller
 {
-    public function index(Request $request) {
-        return Tareas::with('categorias', 'comentarios')->get();
-        return Cache::remember('tareas.all', 60, function () {
-        return Tareas::with('categorias')->get();
-    });
-    }
-
-    public function store(Request $request) {
-        $tareas = Tareas::create([
-            'titulo' => $request->titulo,
-            'cuerpo' => $request->cuerpo,
-            'id_autor' => $request->user()->id,
-            'estado' => 'nuevo'
-        ]);
-
-        $tareas->categorias()->attach($request->categorias_ids);
-
-        Http::post(config('services.api_historial.url').'/log', [
-            'titulo_tareas' => $tareas->titulo,
-            'estado' => $tareas->estado,
-            'id_usuario' => $tareas->id_autor
-        ]);
-
-        return response()->json($tareas, 201);
-    }
-
+    //
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,14 @@
-<php?
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller;
+<?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareasController;
+
+Route::get('/tareas', [TareasController::class, 'index']);
+Route::get('/tareas/{id}', [TareasController::class, 'show']);
+
+Route::middleware('auth.api')->group(function () {
+    Route::post('/tareas', [TareasController::class, 'store']);
+    Route::put('/tareas/{id}', [TareasController::class, 'update']);
+    Route::delete('/tareas/{id}', [TareasController::class, 'destroy']);
+    Route::post('/tareas/{id}/comentarios', [TareasController::class, 'addComentarios']);
+});
